c59e03297ef7a6ea57f313297ebf5621e46ead7d
Matheus-Godinho <matheus.brc22@gmail.com>
2020-11-22 01:43:34 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -12,8 +12,8 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -21,19 +21,25 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products;
+	private Set<Category> categories;
 	
-	public Category() {
-		products = new HashSet<>();
+	public Product() {
+		categories = new HashSet<>();
 	}
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -46,8 +52,26 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Set<Product> getProducts() {
-		return products;
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public Set<Category> getCategories() {
+		return categories;
 	}
 	
 	@Override
@@ -65,7 +89,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -75,3 +99,4 @@ public class Category implements Serializable {
 	}
 
 }
+
